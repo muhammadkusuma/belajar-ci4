0a6d63e692f0667baaa68de3fbc95dbef9cc4d47
muhammadkusuma <mwiraadekusuma1@gmail.com>
2023-02-25 15:32:42 +0700

    Integrasi Template Dinamis

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,14 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $data=[
+            'judul' => 'Homepage'
+        ];
+        // Load the view
+        echo view('templates/v_header', $data);
+        echo view('templates/v_sidebar');
+        echo view('templates/v_topbar');
+        echo view('home/index');
+        echo view('templates/v_footer');
     }
 }
